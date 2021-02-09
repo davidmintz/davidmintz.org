1b28695240a6a44875e38a2c0c0983a1f7d241eb
david <david@davidmintz.org>
2021-02-09 00:23:23 -0500

    add composer.* files, start fetching and caching weather data

---
+++ public/index.php
@@ -0,0 +1,132 @@
+<?php require '../vendor/autoload.php' ;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
+use GuzzleHttp\Client;
+
+$cache_dir = __DIR__.'/../data/cache';
+$cache = new FilesystemAdapter('', 600, __DIR__.'/../data/cache');
+
+$weather = $cache->get('weather_data',function(ItemInterface $item) use ($cache_dir) {
+    try {
+
+        $config = (require(__DIR__.'/../config/config.local.php'))['weather'];    
+        $client = new Client();    
+        $response = $client->get("https://api.openweathermap.org/data/2.5/weather?q=Oak+Bluffs,MA,US&units=imperial&appid={$config['api_key']}");
+        $status = $response->getStatusCode();
+        if (200 == $status) {
+            return json_decode((string)$response->getBody());
+        }
+    } catch (\Exception $e) {
+
+    }
+});
+/**
+ stdClass Object
+(
+    [coord] => stdClass Object
+        (
+            [lon] => -70.562
+            [lat] => 41.4543
+        )
+
+    [weather] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [id] => 800
+                    [main] => Clear
+                    [description] => clear sky
+                    [icon] => 01n
+                )
+
+        )
+
+    [base] => stations
+    [main] => stdClass Object
+        (
+            [temp] => 19.17
+            [feels_like] => 9.66
+            [temp_min] => 12.2
+            [temp_max] => 27
+            [pressure] => 1031
+            [humidity] => 92
+        )
+
+    [visibility] => 10000
+    [wind] => stdClass Object
+        (
+            [speed] => 7.56
+            [deg] => 315
+        )
+
+    [clouds] => stdClass Object
+        (
+            [all] => 1
+        )
+
+    [dt] => 1612845742
+    [sys] => stdClass Object
+        (
+            [type] => 1
+            [id] => 5009
+            [country] => US
+            [sunrise] => 1612784761
+            [sunset] => 1612822018
+        )
+
+    [timezone] => -18000
+    [id] => 4946056
+    [name] => Oak Bluffs
+    [cod] => 200
+)
+ */
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+    
+    <title>David Mintz, homo sapiens</title>
+  </head>
+  <body>
+      <div class="container">
+        <div id="banner" class="row rounded-3 mt-1 text-center d-flex">
+            <div class="col align-self-center">
+                <h1  class="mb-0">David Mintz</h1>
+            <h3 class="mt-0"><em>homo sapiens</em></h3>
+            </div>
+        </div>
+        <div class="row">
+
+            <main class="col-md-7 offset-md-3 pt-3">
+                <p>
+                <?php if ($weather):?>
+                    As of <?php echo date('g:i a',$weather->dt) ?> the local temperature is about <?=$weather->main->temp ?> &deg; F.
+                <?php endif;?>
+                </p>
+                 <p>If you are looking for the David Mintz who grew up in Silver Spring, Maryland; lived in Chile as a teenager; 
+                     got degrees in classical guitar at the Hartt School of Music and at the University of Arizona; became a 
+                    Spanish&lt;&gt;English staff court interpreter for the US District Court in New York City; and retired from the 
+                    federal judiciary in July 2020 &mdash; this is the one.</p>
+                    
+                    <p>My wife Amy Hartford and I live with our dog and three cats in Oak Bluffs, on Martha's Vineyard, 
+                        an island south of Cape Cod, Massachussetts.</p>
+            </main>
+
+        </div>
+      </div>
+    
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- or: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
