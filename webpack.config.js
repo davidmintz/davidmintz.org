3125994bc6c17a6bcd33daba1e111338e1be7322
david <david@davidmintz.org>
2026-01-14 22:45:15 -0500

    update webpack config for /music css

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('weather','./assets/weather.js')
+    .addEntry('music', './assets/music.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
